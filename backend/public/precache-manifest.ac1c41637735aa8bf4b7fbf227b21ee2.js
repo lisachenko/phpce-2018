9059dc314ea172a2b8a2bdf5615a8153d2425b7d
Lisachenko Alexander <lisachenko.it@gmail.com>
2018-10-16 00:44:50 +0600

    Replace backend public files with production-ready generated frontend

---
+++ backend/public/precache-manifest.ac1c41637735aa8bf4b7fbf227b21ee2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b60261c9f35501908c36",
+    "url": "/js/chunk-59d6ebb1.0f7509eb.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9f877824b1590927cf1b",
+    "url": "/js/chunk-62ccef82.3f462b50.js"
+  },
+  {
+    "revision": "aeca0c7e0b030d886dd5",
+    "url": "/js/chunk-2882bfd7.5747028e.js"
+  },
+  {
+    "revision": "598e3fc3771106f27865",
+    "url": "/js/chunk-2d0d6d35.e8196454.js"
+  },
+  {
+    "revision": "2c4d5f770f6499b0e90f",
+    "url": "/js/chunk-2d2086b7.88645517.js"
+  },
+  {
+    "revision": "f3786e5ab4d6a6e4d5d6",
+    "url": "/js/chunk-2d217357.28139be7.js"
+  },
+  {
+    "revision": "35f1bb8e33e66cc86773",
+    "url": "/js/chunk-3e5d063a.b8c33cd1.js"
+  },
+  {
+    "revision": "efe09d299b2566a6690c",
+    "url": "/js/chunk-526d5120.66a50ae3.js"
+  },
+  {
+    "revision": "cae56b71a9718d3274d2",
+    "url": "/js/chunk-vendors.d21dea09.js"
+  },
+  {
+    "revision": "e18886f61b85fde27c14",
+    "url": "/js/chunk-2d0b3289.671214e8.js"
+  },
+  {
+    "revision": "28d1631a1ad87ba8a474",
+    "url": "/js/chunk-6835bd68.686c1813.js"
+  },
+  {
+    "revision": "34568d420ec394d1e260",
+    "url": "/js/chunk-68f6cc30.5a522f7e.js"
+  },
+  {
+    "revision": "4b10c6226283df6f6c08",
+    "url": "/js/chunk-6e6df732.eabf94b1.js"
+  },
+  {
+    "revision": "ff5756103671e8363c73",
+    "url": "/js/chunk-712fd3af.8c885d81.js"
+  },
+  {
+    "revision": "ac2e37a8c844742f8eb1",
+    "url": "/js/app.6b392898.js"
+  },
+  {
+    "revision": "5e05bcbebb0cacae8ad2b0ed4091403e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac2e37a8c844742f8eb1",
+    "url": "/css/app.6fb76ca5.css"
+  }
+];
