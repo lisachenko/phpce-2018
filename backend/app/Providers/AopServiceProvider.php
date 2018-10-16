1e986c11ed41e71ecd5af9de7dfdb73d5569c43e
Lisachenko Alexander <lisachenko.it@gmail.com>
2018-10-16 23:56:17 +0600

    Add AopServiceProvider to register application aspects

---
+++ backend/app/Providers/AopServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AopServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
