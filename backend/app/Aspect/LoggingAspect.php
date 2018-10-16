a55cfd3f92351a11ca4c1bee18059a4acb542506
Lisachenko Alexander <lisachenko.it@gmail.com>
2018-10-17 02:29:17 +0600

    Add simple LoggingAspect

---
+++ backend/app/Aspect/LoggingAspect.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Aspect;
+
+use Go\Aop\Aspect;
+use Go\Aop\Intercept\MethodInvocation;
+use Go\Lang\Annotation\Before;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Application logging aspect
+ *
+ * ./app/Aspect/LoggingAspect.php
+ */
+class LoggingAspect implements Aspect
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Writes a log info before method execution
+     *
+     * @param MethodInvocation $invocation
+     * @Before("execution(public App\**->*(*))")
+     */
+    public function beforeMethod(MethodInvocation $invocation)
+    {
+        $this->logger->info($invocation, $invocation->getArguments());
+    }
+}
